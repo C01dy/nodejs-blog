7c0f02bdcd32b8a1a84c42216e75795459ebb9aa
vladm <vlad.moskvichev@progforce.com>
2022-05-03 23:08:30 +0300

    update and refactoring query builder logic

--- src/utils/queryBuilder.js
+++ src/utils/queryBuilder.js
@@ -6,55 +6,102 @@ function isObject(value) {
   )
 }
 
+class ConditionFactory {
+  constructor(conditionName, leftOperand, operator, rightOperand) {
+    this.conditionName = conditionName
+    this.leftOperand = leftOperand
+    this.operator = operator
+    this.rightOperand = rightOperand
+  }
+
+  createSql() {
+    // if (typeof rightOperand === "number" && isObject(this.rightOperand)) {
+    return ` ${this.conditionName} "${this.leftOperand}" ${this.operator} ${this.rightOperand}`
+    // } else {
+    // return ` ${this.conditionName} "${this.leftOperand}" ${this.operator} '${this.rightOperand}'`
+    // }
+  }
+}
+
 class Query {
-  constructor(table) {
+  constructor(table, isSubquery) {
+    this.isSubquery = !!isSubquery
     this.table = table
-    this.resultSqlString = ``
+    this.resultSqlConditionsArray = []
     this.conditions = []
-    this.comprasionOperators = ["<", ">", "<=", ">=", "=", "<>", "!="]
+    this.cursor = {}
   }
 
   select(fieldsToSelect) {
-    let fields = [] || ["*"]
-    for (let i = 0; i < fieldsToSelect.length; i++) {
-      let fieldName = fieldsToSelect[i]
-      fields.push(`"${fieldName}"`)
-    }
+    if (fieldsToSelect) {
+      let fields = []
+      for (let i = 0; i < fieldsToSelect.length; i++) {
+        let fieldName = fieldsToSelect[i]
+        if (fieldName.includes(".")) {
+          let [left, right] = fieldName.split(".")
+          left = `"${left}"`
+          fields.push(`${left}.${right}`)
+        } else {
+          fields.push(`"${fieldName}"`)
+        }
+      }
 
-    this.resultSqlString += `SELECT ${fields.join(", ")} FROM "${this.table}"`
+      this.resultSqlConditionsArray.unshift(
+        `SELECT ${fields.join(", ")} FROM "${this.table}"`
+      )
+    } else {
+      this.resultSqlConditionsArray.unshift(`SELECT * FROM "${this.table}"`)
+    }
 
     return this
   }
 
-  where(...args) {
-    if (args.length === 3 && this.comprasionOperators.includes(args[1])) {
-      const [propKey, operator, propValue] = args
+  where(leftOperand, operator, rightOperand) {
+    this.resultSqlConditionsArray.push(
+      new ConditionFactory(
+        "WHERE",
+        leftOperand,
+        operator,
+        rightOperand
+      ).createSql()
+    )
+    return this
+  }
 
-      if (typeof propValue === "number") {
-        this.resultSqlString += ` WHERE "${propKey}" ${operator} ${propValue}`
-      } else {
-        this.resultSqlString += ` WHERE "${propKey}" ${operator} '${propValue}'`
-      }
+  andWhere(leftOperand, operator, rightOperand) {
+    this.resultSqlConditionsArray.push(
+      new ConditionFactory(
+        "AND",
+        leftOperand,
+        operator,
+        rightOperand
+      ).createSql()
+    )
+    return this
+  }
 
-      return this
-    } else if (args.length === 1 && isObject(args[0])) {
-      // TODO: logic if is object of conditions
-      return
-    }
+  orWhere(leftOperand, operator, rightOperand) {
+    this.resultSqlConditionsArray.push(
+      new ConditionFactory(
+        "OR",
+        leftOperand,
+        operator,
+        rightOperand
+      ).createSql()
+    )
+    return this
+  }
+
+  toString() {
+    if (this.isSubquery) return `(${this.resultSqlConditionsArray.join("")})`
+    return this.resultSqlConditionsArray.join("")
   }
 
   // TODO: join method
 
+  // TODO: run query logic
   exec() {
-    return this
+    return this.resultSqlConditionsArray
   }
 }
 
-const productTable = new Query("user")
-
-console.log(
-  productTable
-    .select(["first_name", "last_name"])
-    .where("first_name", "=", "Joe")
-    .exec()
-)
